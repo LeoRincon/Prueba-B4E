ecc4aa9efef26ed656d1f038267e0d830ea90109
Leonardo Rincón <jleobotache@gmail.com>
2021-07-27 12:38:42 -0500

    fixhot

--- src/utils/validateNumberPositive.js
+++
@@ -1,8 +0,0 @@
-export const validateNumberPositive = (param) => {
-  if (Math.sign(param) === 1) {
-    percentage.classList.add('positive');
-  } else {
-    percentage.classList.add('negative');
-  }
-  return param;
-};
--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,6 +1,3 @@
-const URL_BASE_CRYPTOTRACKER = 'https://cryptotrackerapi.herokuapp.com/';
 const CRYPTOS_PER_PAGES = 20;
 
 export const BASE_URL_COINGECKO = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${CRYPTOS_PER_PAGES}&page=1&sparkline=false`;
-
-export const URL_BALANCE = `${URL_BASE_CRYPTOTRACKER}balance/`;
